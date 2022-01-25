69be427cb24d05956ce2b009af6dc5f39e42d17e
George T <n35.nnlpd@slmail.me>
2022-01-25 15:29:20 +0000

    Core almost complete

--- spec/cohort-manager-test.js
+++ spec/cohort-manager-test.js
@@ -1,107 +1,159 @@
-const CohortManager = require('../src/cohort-manager.js')
+const CohortManager = require("../src/cohort-manager.js");
 
-describe('Cohort Manager', () => {
-  let cohortManager
+describe("Cohort Manager", () => {
+  let cohortManager;
 
   beforeEach(() => {
-    cohortManager = new CohortManager()
-  })
+    cohortManager = new CohortManager();
+  });
 
   // Create a cohort with a cohort name
-  it('Create a cohort with a name', () => {
+  it("Create a cohort with a name", () => {
     // setup
-    const cohortName = 'Cohort Four'
+    const cohortName = "Cohort Four";
     // execute
-    cohortManager.createCohort(cohortName)
+    cohortManager.createCohort(cohortName);
     // check
     // check cohort has been put on cohorts list
-    expect(cohortManager.cohorts.length).toEqual(1)
+    expect(cohortManager.cohorts.length).toEqual(1);
     // if it has check if name is in the list
-    expect(cohortManager.cohorts[0].cohortName).toEqual(cohortName)
-  })
+    expect(cohortManager.cohorts[0].cohortName).toEqual(cohortName);
+  });
 
   // Search for a cohort by cohort cohortName
-  it('Search for cohort by name', () => {
+  it("Search for cohort by name", () => {
     // setup
-    const expected = { cohortName: 'Cohort Five', students: [] }
+    const expected = { cohortName: "Cohort Five", students: [] };
     // execute
-    cohortManager.createCohort('Cohort One')
-    cohortManager.createCohort('Cohort Two')
-    cohortManager.createCohort('Cohort Three')
-    cohortManager.createCohort('Cohort Four')
-    cohortManager.createCohort('Cohort Five')
+    cohortManager.createCohort("Cohort One");
+    cohortManager.createCohort("Cohort Two");
+    cohortManager.createCohort("Cohort Three");
+    cohortManager.createCohort("Cohort Four");
+    cohortManager.createCohort("Cohort Five");
     // cohortManager.searchCohorts("Cohort Five");
     // check
     // called inlining
-    expect(cohortManager.searchCohorts('Cohort Five')).toEqual(expected)
-  })
+    expect(cohortManager.searchCohorts("Cohort Five")).toEqual(expected);
+  });
 
   // Add student to a specific cohort manager
-  it('Add student to specific cohort', () => {
+  it("Add student to specific cohort", () => {
     // setup
     // const cohortName = "Cohort Four";
     // hardcode
     const expected = {
       id: 100,
-      name: 'Michael',
-      surname: 'Johnson',
-      github: 'mj100',
-      email: 'mj100@aol.net'
-    }
+      name: "Michael",
+      surname: "Johnson",
+      github: "mj100",
+      email: "mj100@aol.net",
+    };
     // execute
-    cohortManager.createCohort('Cohort Four')
-    cohortManager.createCohort('Cohort Five')
+    cohortManager.createCohort("Cohort Four");
+    cohortManager.createCohort("Cohort Five");
 
     const result = cohortManager.addStudent(
-      'Cohort Four',
+      "Cohort Four",
       100,
-      'Michael',
-      'Johnson',
-      'mj100',
-      'mj100@aol.net'
-    )
+      "Michael",
+      "Johnson",
+      "mj100",
+      "mj100@aol.net"
+    );
 
     // cohortManager.[0].cohortName
     // check
-    expect(result).toEqual(expected)
-
-  })
+    expect(result).toEqual(expected);
+  });
 
   // Create a cohort with a cohort name
-  it('Remove a cohort by cohort name', () => {
+  it("Remove a cohort by cohort name", () => {
     // setup
-    const expected = [{ cohortName: 'Cohort One', students: [  ] }, { cohortName: 'Cohort Three', students: [  ] }]
+    const expected = [
+      { cohortName: "Cohort One", students: [] },
+      { cohortName: "Cohort Three", students: [] },
+    ];
     // execute
-    cohortManager.createCohort('Cohort One')
-    cohortManager.createCohort('Cohort Two')
-    cohortManager.createCohort('Cohort Three')
+    cohortManager.createCohort("Cohort One");
+    cohortManager.createCohort("Cohort Two");
+    cohortManager.createCohort("Cohort Three");
     // check
-    const result = cohortManager.removeCohort('Cohort Two')
+    const result = cohortManager.removeCohort("Cohort Two");
 
-    expect(result).toEqual(expected)
-  })
+    expect(result).toEqual(expected);
+  });
 
-  it('Remove a student by id', () => {
-
+  it("Remove a student by id", () => {
+    // setup
     const expected = {
-      cohortName: 'Cohort Four',
-      students: [{
-        id: 700,
-        name: 'James',
-        surname: 'Wheeler',
-        github: 'jwheel7',
-        email: 'jwheel7@provider.com'
-      },
-      {
-        id: 900,
-        name: firstName,
-        surname: lastName,
-        github: gitName,
-        email: email
-      }]
-    }
-  }
-})
+      cohortName: "Cohort Four",
+      students: [
+        {
+          id: 700,
+          name: "James",
+          surname: "Wheeler",
+          github: "jwheel7",
+          email: "jwheel7@provider.com",
+        },
+        {
+          id: 900,
+          name: "Morris",
+          surname: "Miner",
+          github: "morris900",
+          email: "mmorris9@internet.biz",
+        },
+      ],
+    };
+    // execute
+    cohortManager.createCohort("Cohort Four");
+    // cohortManager.createCohort('Cohort Five')
+    cohortManager.addStudent(
+      "Cohort Four",
+      700,
+      "James",
+      "Wheeler",
+      "jwheel7",
+      "jwheel7@provider.com"
+    );
+    cohortManager.addStudent(
+      "Cohort Four",
+      900,
+      "Morris",
+      "Miner",
+      "morris900",
+      "mmorris9@internet.biz"
+    );
+    cohortManager.addStudent(
+      "Cohort Four",
+      1000,
+      "Graham",
+      "Sharp",
+      "gshapr5",
+      "gsharp5@coldmail.org"
+    );
+    cohortManager.removeStudent(1000, "Cohort Four");
+  });
+
+  it("Return error if student not found", () => {
+    const expected = "Cohort Fifteen not found";
+
+    cohortManager.createCohort("Cohort One");
+    cohortManager.createCohort("Cohort Two");
+    cohortManager.createCohort("Cohort Three");
+
+    expect(cohortManager.searchCohorts("Cohort Fifteen")).toEqual(expected);
+  });
+
+  it("Return error if student not found", () => {
+    const expected = "Cohort Fifteen not found";
+
+    cohortManager.createCohort("Cohort One");
+    cohortManager.createCohort("Cohort Two");
+    cohortManager.createCohort("Cohort Three");
+
+    expect(cohortManager.searchCohorts("Cohort Fifteen")).toEqual(expected);
+  });
+});
 
 // The Cohort Manager should be able to support the following interactions
 
@@ -112,4 +164,4 @@ describe('Cohort Manager', () => {
 // - Remove student from a specific cohort
 // - Return errors if student or cohort not found
 
-// A cohort should have a list of students. Each student should have a studentID, first name, last name, github username, email.
+// A cohort should have a list of students. Each student should have a studentID, first name, last name, github username, email
--- src/cohort-manager.js
+++ src/cohort-manager.js
@@ -1,61 +1,74 @@
 class CohortManager {
-  constructor (name) {
-    this.cohorts = []
+  constructor(name) {
+    this.cohorts = [];
     // property of the class
-    this.name = name
+    this.name = name;
     // include the attributes that you would want every instance of the class
     // CohortManager to have
   }
 
-  createCohort (name) {
+  createCohort(name) {
     const cohort = {
       cohortName: name,
-      students: []
-    }
+      students: [],
+    };
 
-    this.cohorts.push(cohort)
+    this.cohorts.push(cohort);
   }
 
-  searchCohorts (cohortName) {
+  searchCohorts(cohortName) {
     for (let i = 0; i < this.cohorts.length; i++) {
       if (this.cohorts[i].cohortName === cohortName) {
-        return this.cohorts[i]
+        return this.cohorts[i];
       }
     }
+    return cohortName + " not found";
   }
 
-  addStudent (cohortName, ID, firstName, lastName, gitName, email) {
-
-    const cohort = this.searchCohorts (cohortName)
+  addStudent(cohortName, ID, firstName, lastName, gitName, email) {
+    const cohort = this.searchCohorts(cohortName);
 
     const student = {
       id: ID,
       name: firstName,
       surname: lastName,
       github: gitName,
-      email: email
-    }
+      email: email,
+    };
 
-    cohort.students.push(student)
-    return student
+    cohort.students.push(student);
+    return student;
   }
 
-  removeCohort (name) {
-
+  removeCohort(name) {
     for (let i = 0; i < this.cohorts.length; i++) {
       if (this.cohorts[i].cohortName === name) {
-        this.cohorts.splice(i, 1)
+        this.cohorts.splice(i, 1);
+      }
+      return this.cohorts;
+    }
+    return name + " not found";
+  }
+
+  // find cohort (function) find student in student array then remove student
+  // going through list of students
+  removeStudent(ID, cohortName) {
+    const cohort = this.searchCohorts(cohortName);
+
+    for (let i = 0; i < cohort.students.length; i++) {
+      if (cohort.students[i].id === ID) {
+        cohort.students.splice(i, 1);
       }
     }
-    return this.cohorts
-  }
+    return cohort;
 
-// find cohort (function) find student in student array then remove student
-// going through list of students
-  removeStudent (name) {
-    this.searchCohorts(name)
+    // const cohort = this.searchCohorts(cohortName)
+    //   for (let i = 0; i < cohort.cohortName.students.length; i++){
+    //     if ((this.cohorts.students.id) === ID)
+    //     this.cohorts.splice(id, 1)
+    // }
+    // return this.cohorts.cohortName.students
   }
-
 }
 
-module.exports = CohortManager
+module.exports = CohortManager;
