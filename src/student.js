e264089dcf2714e72622188b178bd9164e39d434
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-30 14:16:30 +0200

    created student.js

---
+++ src/student.js
@@ -0,0 +1,10 @@
+class Student {
+  constructor(id, firstName, lastName, githubName, email) {
+    this.id = id;
+    this.fullName = `${firstName} ${lastName}`;
+    this.githubName = githubName;
+    this.email = email;
+  }
+}
+
+module.exports = Student;
