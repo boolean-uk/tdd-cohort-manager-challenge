2e34d304b1f481832a1a52b5f563a5a66a663898
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-03-31 18:38:33 +0200

    completed extended spec

--- spec/cohortmanager.spec.js
+++ spec/cohortmanager.spec.js
@@ -223,4 +223,136 @@ it("cohorts have fixed capacity at 2 students. Adding students is not possible b
     expect(resultFour).toEqual(maxNumberOfStudents)
     })
 
+it("Cohorts can't have the same name", ()  => {
+    // set up
+    const cohortManager = new CohortManager()
+    const expected = false
+
+    // execute
+    cohortManager.addCohort("cohortOne", 2)
+    const result = cohortManager.addCohort("cohortOne", 7)
+
+    // verify
+    expect(result).toEqual(expected)
+
+    })
+
+it("Cohorts can't exist without a name", ()  => {
+    // set up
+    const cohortManager = new CohortManager()
+    const expected = false
+
+    // execute
+    const result = cohortManager.addCohort("", 2)
+
+    // verify
+    expect(result).toEqual(expected)
+
+    })
+
+it("same student can't exist in multiple cohorts", ()  => {
+    // set up
+    const cohortManager = new CohortManager()
+    const expected = false
+
+    // execute
+    cohortManager.addCohort("cohortOne", 2)
+    cohortManager.addCohort("cohortTwo", 2)
+    cohortManager.addStudentToCohort("cohortOne", "Ngolo", "Kante", "kantecodes", "kantecodes@gmail.com")
+    const result = cohortManager.addStudentToCohort("cohortTwo", "Ngolo", "Kante", "kantecodes", "kantecodes@gmail.com")
+
+    // verify
+    expect(result).toEqual(expected)
+    })
+
+it("remove student that exists from a cohort that they are not in", ()  => {
+    // set up
+    const cohortManager = new CohortManager()
+    const expected = false
+    // execute
+    cohortManager.addCohort("cohortOne", 2)
+    cohortManager.addStudentToCohort("cohortOne", "Ngolo", "Kante", "kantecodes", "kantecodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortTwo", "Reece", "James", "reececodes", "reececodes@gmail.com")
+    const result = cohortManager.removeStudentFromCohort("cohortOne", "reececodes@gmail.com")
+    // verify
+    expect(result).toEqual(expected)
+    })
+
+it("Search for students by first name and return all matching results", ()  => {
+    // set up
+    const cohortManager = new CohortManager()
+    const expected = [new Student("cohortOne", "Ngolo", "Kante", "kantecodes", "kantecodes@gmail.com", 1), new Student("cohortTwo", "Ngolo", "Smith", "nscodes", "nscodes@gmail.com", 8)]
+    // execute
+    cohortManager.addCohort("cohortOne", 5)
+    cohortManager.addCohort("cohortTwo", 5)
+    cohortManager.addStudentToCohort("cohortOne", "Ngolo", "Kante", "kantecodes", "kantecodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortOne", "Reece", "James", "reececodes", "reececodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortOne", "Dan", "James", "jamescodes", "jamescodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortOne", "Dan", "Kante", "dankcodes", "dankcodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortOne", "Reece", "Kante", "rkcodes", "rkcodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortTwo", "Adam", "Smith", "adamscodes", "adamscodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortTwo", "Adam", "Jones", "adamjcodes", "adamjcodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortTwo", "Ngolo", "Smith", "nscodes", "nscodes@gmail.com")
+    const result = cohortManager.searchStudentByFirstName("Ngolo")
+
+    // verify
+    expect(result).toEqual(expected)
+    })
+
+it("Search for students by surname and return all matching results", ()  => {
+    // set up
+    const cohortManager = new CohortManager()
+    const expected = [new Student("cohortOne", "Reece", "Jones", "reececodes", "reececodes@gmail.com", 2), new Student("cohortOne", "Dan", "Jones", "jonescodes", "jamescodes@gmail.com", 3), new Student("cohortTwo", "Adam", "Jones", "adamjcodes", "adamjcodes@gmail.com", 7)]
+    // execute
+    cohortManager.addCohort("cohortOne", 5)
+    cohortManager.addCohort("cohortTwo", 5)
+    cohortManager.addStudentToCohort("cohortOne", "Ngolo", "Kante", "kantecodes", "kantecodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortOne", "Reece", "Jones", "reececodes", "reececodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortOne", "Dan", "Jones", "jonescodes", "jamescodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortOne", "Dan", "Kante", "dankcodes", "dankcodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortOne", "Reece", "Kante", "rkcodes", "rkcodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortTwo", "Adam", "Smith", "adamscodes", "adamscodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortTwo", "Adam", "Jones", "adamjcodes", "adamjcodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortTwo", "Ngolo", "Smith", "nscodes", "nscodes@gmail.com")
+    const result = cohortManager.searchStudentBySurname("Jones")
+
+    // verify
+    expect(result).toEqual(expected)
+    })
+
+it("Search for students by first name and surname and return all matching results", ()  => {
+    // set up
+    const cohortManager = new CohortManager()
+    const expected = [new Student("cohortOne", "Ngolo", "Kante", "kantecodes", "kantecodes@gmail.com", 1)]
+    // execute
+    cohortManager.addCohort("cohortOne", 5)
+    cohortManager.addCohort("cohortTwo", 5)
+    cohortManager.addStudentToCohort("cohortOne", "Ngolo", "Kante", "kantecodes", "kantecodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortOne", "Reece", "James", "reececodes", "reececodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortOne", "Dan", "James", "jamescodes", "jamescodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortOne", "Dan", "Kante", "dankcodes", "dankcodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortOne", "Reece", "Kante", "rkcodes", "rkcodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortTwo", "Adam", "Smith", "adamscodes", "adamscodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortTwo", "Adam", "Jones", "adamjcodes", "adamjcodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortTwo", "Ngolo", "Smith", "nscodes", "nscodes@gmail.com")
+    const result = cohortManager.searchStudentByFullName("Ngolo", "Kante")
+
+    // verify
+    expect(result).toEqual(expected)
+    })
+
+it("Search for students by first name that does not exist return all matching results", ()  => {
+    // set up
+    const cohortManager = new CohortManager()
+    const expected = false
+    // execute
+    cohortManager.addCohort("cohortOne", 5)
+    cohortManager.addCohort("cohortTwo", 5)
+    cohortManager.addStudentToCohort("cohortOne", "Ngolo", "Kante", "kantecodes", "kantecodes@gmail.com")
+    cohortManager.addStudentToCohort("cohortTwo", "Ngolo", "Smith", "nscodes", "nscodes@gmail.com")
+    const result = cohortManager.searchStudentByFirstName("Alex")
+
+    // verify
+    expect(result).toEqual(expected)
+    })
 })
--- src/cohort-manager.js
+++ src/cohort-manager.js
@@ -1,74 +1,129 @@
-const Cohort = require('./cohort')
-const Student = require('./student')
+const Cohort = require("./cohort");
+const Student = require("./student");
 
 class CohortManager {
-  constructor () {
-    this.cohortArray = []
-    this.studentID = 1
+  constructor() {
+    this.cohortArray = [];
+    this.studentID = 1;
   }
 
-  addCohort (name, maxCapacity) {
-    const cohort = new Cohort(name, maxCapacity)
-    for (let i = 0; i < this.cohortArray.length; i++) { if (name === this.cohortArray[i].name) { return false } }
-    this.cohortArray.push(cohort)
-    return this.cohortArray
+  addCohort(name, maxCapacity) {
+    if (name.length === 0) {return false}
+    for (let i = 0; i < this.cohortArray.length; i++) {
+      if (name === this.cohortArray[i].name) {return false}
+    }
+    const cohort = new Cohort(name, maxCapacity);
+    this.cohortArray.push(cohort);
+    return this.cohortArray;
   }
 
-  removeCohort (name) {
+  removeCohort(name) {
     for (let i = 0; i < this.cohortArray.length; i++) {
       if (name === this.cohortArray[i].name) {
-        this.cohortArray.splice(i, 1)
-        return this.cohortArray
+        this.cohortArray.splice(i, 1);
+        return this.cohortArray;
       }
-    } return false
+    }
+    return false;
   }
 
-  searchByCohortName (name) {
+  searchByCohortName(name) {
     for (let i = 0; i < this.cohortArray.length; i++) {
-      if (name === this.cohortArray[i].name) { return this.cohortArray[i] }
-    } return 'This cohort does not exist'
-  }
-
-  searchByStudentEmail (email) {
-    for (let i = 0; i < this.cohortArray.length; i++) {
-      for (let j = 0; j < this.cohortArray[i].studentsArray.length; j++) {
-        if (email === this.cohortArray[i].studentsArray[j].email) { return this.cohortArray[i].studentsArray[j] }
+      if (name === this.cohortArray[i].name) {
+        return this.cohortArray[i];
       }
-    } return 'There are no students with the entered email'
+    }
+    return "This cohort does not exist";
   }
 
-  searchByStudentID (studentID) {
+  searchByStudentEmail(email) {
     for (let i = 0; i < this.cohortArray.length; i++) {
       for (let j = 0; j < this.cohortArray[i].studentsArray.length; j++) {
-        if (studentID === this.cohortArray[i].studentsArray[j].studentID) { return this.cohortArray[i].studentsArray[j] }
+        if (email === this.cohortArray[i].studentsArray[j].email) {
+          return this.cohortArray[i].studentsArray[j];
+        }
       }
-    } return 'There are no students with the entered email'
+    }
+    return "There are no students with the entered email";
   }
 
-  addStudentToCohort (cohortName, firstName, surname, githubUser, email) {
-    const student = new Student(cohortName, firstName, surname, githubUser, email)
+  searchByStudentID(studentID) {
+    for (let i = 0; i < this.cohortArray.length; i++) {
+      for (let j = 0; j < this.cohortArray[i].studentsArray.length; j++) {
+        if (studentID === this.cohortArray[i].studentsArray[j].studentID) {
+          return this.cohortArray[i].studentsArray[j];
+        }
+      }
+    }
+    return "There are no students with the entered email";
+  }
+
+  addStudentToCohort(cohortName, firstName, surname, githubUser, email) {
+    if (this.searchByStudentEmail(email) !== "There are no students with the entered email"){return false}
+    const student = new Student(cohortName,firstName,surname,githubUser,email);
     for (let i = 0; i < this.cohortArray.length; i++) {
       if (cohortName === this.cohortArray[i].name && this.cohortArray[i].studentsArray.length < this.cohortArray[i].maxCapacity) {
-        student.studentID = this.studentID
-        this.studentID++
-        this.cohortArray[i].studentsArray.push(student)
-        return student
+        student.studentID = this.studentID;
+        this.studentID++;
+        this.cohortArray[i].studentsArray.push(student);
+        return student;
       }
-    } return false
+    }
+    return false;
   }
 
-  removeStudentFromCohort (cohortName, email) {
+  removeStudentFromCohort(cohortName, email) {
     for (let i = 0; i < this.cohortArray.length; i++) {
       if (cohortName === this.cohortArray[i].name) {
         for (let j = 0; j < this.cohortArray[i].studentsArray.length; j++) {
           if (email === this.cohortArray[i].studentsArray[j].email) {
-            this.cohortArray[i].studentsArray.splice(j, 1)
-            return this.cohortArray[i].studentsArray
+            this.cohortArray[i].studentsArray.splice(j, 1);
+            return this.cohortArray[i].studentsArray;
           }
         }
       }
-    } return false
+    }
+    return false;
+  }
+
+  searchStudentByFirstName(firstName){
+    let matchedFirstNameArray = []
+    for (let i = 0; i < this.cohortArray.length; i++) {
+      for (let j = 0; j < this.cohortArray[i].studentsArray.length; j++) {
+        if(this.cohortArray[i].studentsArray[j].firstName === firstName) {
+          matchedFirstNameArray.push(this.cohortArray[i].studentsArray[j])
+        }
+      }
+    }
+    if (matchedFirstNameArray.length === 0){return false}
+    return matchedFirstNameArray
+  }
+
+  searchStudentBySurname(surname){
+    let matchedSurnameArray = []
+    for (let i = 0; i < this.cohortArray.length; i++) {
+      for (let j = 0; j < this.cohortArray[i].studentsArray.length; j++) {
+        if(this.cohortArray[i].studentsArray[j].surname === surname) {
+          matchedSurnameArray.push(this.cohortArray[i].studentsArray[j])
+        }
+      }
+    }
+    if (matchedSurnameArray.length === 0){return false}
+    return matchedSurnameArray
+  }
+
+  searchStudentByFullName(firstName, surname){
+    let matchedFullNameArray = []
+    for (let i = 0; i < this.cohortArray.length; i++) {
+      for (let j = 0; j < this.cohortArray[i].studentsArray.length; j++) {
+        if(this.cohortArray[i].studentsArray[j].surname === surname && this.cohortArray[i].studentsArray[j].firstName === firstName){
+          {matchedFullNameArray.push(this.cohortArray[i].studentsArray[j])}
+        }
+      }
+    }
+    if (matchedFullNameArray.length === 0){return false}
+    return matchedFullNameArray
   }
 }
 
-module.exports = CohortManager
+module.exports = CohortManager;
