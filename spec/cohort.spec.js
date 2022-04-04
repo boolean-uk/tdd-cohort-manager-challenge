43dabbe72d290c780a3bed2cf1e694545cd23c99
Anica Rahman <anicarahman2009@yahoo.it>
2022-04-04 17:56:15 +0100

    Added two students

--- spec/cohort.spec.js
+++ spec/cohort.spec.js
@@ -13,6 +13,21 @@ describe('Cohort', () => {
     expect(result).toEqual(expected)
   })
 
+  it('add two students', () => {
+    //set up
+    const cohort = new Cohort('Cohort 1')
+    const sarah = new Student(2, 'Sarah', 'Malik', 'Sarahhh', 'sarah@gmail.com')
+    const david = new Student(3, 'David', 'C', 'Daviddd', 'david@gmail.com')
+    const expected1 = [sarah, david]
+    const expected2 = [sarah, david]
+    //execute
+    const result1 = cohort.addStudent(sarah)
+    const result2 = cohort.addStudent(david)
+    //verify
+    expect(result1).toEqual(expected1)
+    expect(result2).toEqual(expected2)
+  })
+
   /* it('remove a student', () => {
     //set up
     const cohort = new Cohort()
