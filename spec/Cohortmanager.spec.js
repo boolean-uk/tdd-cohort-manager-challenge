60bca74c5a4d20b5a375f1a6e63135c9952016e1
James Byrne <james.c.byrne1994@gmail.com>
2022-11-11 16:38:32 +0000

    more features added

--- spec/Cohortmanager.spec.js
+++ spec/Cohortmanager.spec.js
@@ -1,3 +1,12 @@
+const Cohortmanager = require('../src/Cohortmanager')
+
 describe('', () => {
   it()
 })
+
+// describe('add a Cohort', () => {
+//   it('should have a cohort added to it', () => {
+//     let cohortList =  Cohort { name: 'cohort 1', studentList: [] }
+//     expect(aNewCohort).toEqual(cohortList)
+//   })
+// })
--- src/Cohortmanager.js
+++ src/Cohortmanager.js
@@ -2,8 +2,7 @@ const Student = require('../src/Student.js')
 const Cohort = require('../src/Cohort.js')
 
 class Cohortmanager {
-  constructor(name, Id) {
-    this.Id = Id
+  constructor(name) {
     this.name = name
     this.cohortList = []
     this.students = []
@@ -35,31 +34,94 @@ class Cohortmanager {
     const newStudent = new Student(Id, firstName, lastNames, github, email)
     this.students.push(newStudent)
   }
+
+  addtoCohort(firstName, name) {
+    const studentToAdd = this.students.find((e) => e.firstName === firstName)
+    const chosenCohort = this.cohortList.find((e) => e.name === name)
+
+    if (studentToAdd === undefined || chosenCohort === undefined) {
+      return 'this student / Cohort does not exist in this cohort'
+    } else {
+      chosenCohort.studentList.push(studentToAdd)
+    }
+  }
+
+  removeCohort(name) {
+    const findCohort = this.cohortList.find((e) => e.name === name)
+
+    if (findCohort === undefined) {
+      return 'This cohort does not exist'
+    } else {
+      const index = this.cohortList.indexOf(findCohort)
+      this.cohortList.splice(index, 1)
+    }
+  }
+
+  removeStudent(firstName) {
+    const findStudent = this.students.find((e) => e.name === firstName)
+    if (findStudent === undefined) {
+      return 'this student does not exist in this cohort'
+    } else {
+      const index = this.cohortList.indexOf(findStudent)
+      this.students.splice(index, 1)
+    }
+  }
 }
 
-const aNewCohort = new Cohortmanager('cohort 1')
-console.log(aNewCohort)
+//////////////////////////////////////////////////////////////////////////////////////////
 
-aNewCohort.createCohort('cohort 1')
-const notFound = aNewCohort.searchCohort('cohort 1')
-const studentTest = aNewCohort.addStudent(
-  1,
-  'James',
-  'Byrne',
-  'Jamesgithub',
-  'james@james.com'
-)
+// console.log tests below
 
-const studentsView = aNewCohort.students
-console.log('This is the list ', aNewCohort.cohortList)
-console.log('is it found ?: ', notFound)
-console.log('student List', studentsView)
-console.log('my studentTest code test ', studentTest)
+// const aNewCohort = new Cohortmanager('cohort 1')
+// const secondCohort = new Cohortmanager('cohort 2')
+// const thirdReoveCohort = new Cohortmanager('cohort 3')
 
-module.export = Cohortmanager
+// // thirdReoveCohort.removeCohort('cohort 3')
+
+// aNewCohort.createCohort('cohort 1')
+// secondCohort.createCohort('cohort 2')
+// thirdReoveCohort.createCohort('cohort 3')
+
+// thirdReoveCohort.removeCohort('cohort 3')
+
+// console.log('first cohort', aNewCohort)
+// console.log('Second cohort : ', secondCohort)
+// console.log('3rd cohort : ', thirdReoveCohort)
+
+// const notFound = aNewCohort.searchCohort('cohort 2')
+// const studentTest = aNewCohort.addStudent(
+//   1,
+//   'James',
+//   'Byrne',
+//   'Jamesgithub',
+//   'james@james.com'
+// )
+// const secondStudent = secondCohort.addStudent(
+//   2,
+//   'Luna',
+//   'Ballona',
+//   'LunaTheCat@github',
+//   'luna@meow.com'
+// )
+
+// const studentsView = aNewCohort.students
+// const studentView2 = secondCohort.students
+
+// const removeStudentTest = secondCohort.addtoCohort('Luna', 'cohort 2')
+// const studentListCohor2 = secondCohort.studentList
+
+// // console.log('This is the list ', aNewCohort.cohortList)
+// // console.log('is it found ?: ', notFound)
+// // console.log('student List is', studentsView)
+// console.log('Second student ', studentView2)
+// console.log(studentListCohor2)
 
 // const studentOne = new Student(1, 'james', 'byrne', 'github', 'james@james.com')
 // console.log(studentOne)
 
 // const newCohort = new Cohort('cohort1')
 // console.log(newCohort)
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+module.export = Cohortmanager
