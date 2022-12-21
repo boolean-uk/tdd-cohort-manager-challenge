63f37c2139f94b3f2fc7df02513fe761d6aa0dec
Fatima <fxtimaahmed14@gmail.com>
2022-12-21 12:18:25 +0000

    Domain model added + new fies created

---
+++ spec/CohortManaager.spec.js
@@ -0,0 +1,1 @@
+// const CohortManager = require('../src/CohortManager.js')
---
+++ src/CohortManager.js
@@ -0,0 +1,8 @@
+class CohortManager {
+  constructor() {
+    this.id = 0
+    this.cohorts = []
+  }
+}
+
+module.exports = CohortManager
