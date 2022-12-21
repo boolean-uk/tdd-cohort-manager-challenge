2ef055512e7b79d15d2ae58f237720435c1613b5
Fatima <fxtimaahmed14@gmail.com>
2022-12-21 12:53:17 +0000

    create cohorts + get All cohorts function made

--- spec/CohortManaager.spec.js
+++ spec/CohortManaager.spec.js
@@ -1,1 +1,20 @@
-// const CohortManager = require('../src/CohortManager.js')
+const CohortManager = require('../src/CohortManager.js')
+
+describe('CohortManager', () => {
+  let CM
+
+  beforeEach(() => {
+    CM = new CohortManager()
+  })
+
+  it('should create a new cohort w/ user input as name', () => {
+    CM.createCohort('coolest cohort')
+    const result = CM.getAllCohorts()
+    expect(result).toEqual([
+      {
+        name: 'coolest cohort',
+        students: []
+      }
+    ])
+  })
+})
--- src/CohortManager.js
+++ src/CohortManager.js
@@ -3,6 +3,19 @@ class CohortManager {
     this.id = 0
     this.cohorts = []
   }
+
+  createCohort(str) {
+    const cohort = {
+      name: str,
+      students: []
+    }
+    this.cohorts.push(cohort)
+    return cohort
+  }
+
+  getAllCohorts() {
+    return this.cohorts
+  }
 }
 
 module.exports = CohortManager
