b7f979c178fe0f88ee6d1f8832868ca7775f2000
MaybeFreak <michaelpope97@gmail.com>
2023-03-08 12:05:32 +0100

    added tests and source code for searchCohort

--- spec/cohortManager.spec.js
+++ spec/cohortManager.spec.js
@@ -30,4 +30,38 @@ describe('The Cohort Manager should be able to', () => {
     //verify
     expect(cm.cohorts).toEqual(expected)
   })
+
+  it('should display an error message if a cohort was already created', () => {
+    //set up
+    cm.createCohort('test1')
+    const expected = 'Cohort already exists'
+    //execute
+    const res = cm.createCohort('test1')
+    //verfiy
+    expect(res).toEqual(expected)
+  })
+
+  it('find a cohort by name', () => {
+    //set up
+    cm.createCohort('test1')
+    cm.createCohort('test2')
+    cm.createCohort('test3')
+    const expected = new Cohort('test2')
+    //execute
+    const res = cm.searchCohort('test2')
+    //verify
+    expect(res).toEqual(expected)
+  })
+
+  it('return an error message if cohort is not found', () => {
+    //set up
+    cm.createCohort('test1')
+    cm.createCohort('test2')
+    cm.createCohort('test3')
+    const expected = 'Cohort not found'
+    //execute
+    const res = cm.searchCohort('test5')
+    //verify
+    expect(res).toEqual(expected)
+  })
 })
--- src/cohortManager.js
+++ src/cohortManager.js
@@ -9,7 +9,20 @@ class CohortManager {
 
   createCohort(name) {
     const cohortToAdd = new Cohort(name)
-    this.cohorts.push(cohortToAdd)
+    const doesNotExist =
+      this.cohorts.find((cohort) => cohort.name === cohortToAdd.name) ===
+      undefined
+    if (doesNotExist) {
+      this.cohorts.push(cohortToAdd)
+    } else {
+      return 'Cohort already exists'
+    }
+  }
+
+  searchCohort(name) {
+    const foundCohort = this.cohorts.find((cohort) => cohort.name === name)
+    if (foundCohort === undefined) return 'Cohort not found'
+    else return foundCohort
   }
 }
 
