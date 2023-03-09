e293e3ffb0c28c50d093b5fab5bcba407ba53a43
Oliver West <oliversimon.west@gmail.com>
2023-03-09 12:20:25 +0000

    fixing env files

--- src/twilio_sms.js
+++ src/twilio_sms.js
@@ -1,11 +1,13 @@
-const accountSid = ''
-const authToken = ''
+require('dotenv').config()
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+
 const client = require('twilio')(accountSid, authToken)
 
 client.messages
   .create({
     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-    from: '',
-    to: ''
+    from: process.env.TWILIO_PHONE,
+    to: process.env.MY_PHONE_NUMBER
   })
   .then((message) => console.log(message.sid))
