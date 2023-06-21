8a892d65a16ac9f8c6a993e1e79363b97bec7986
rockettdev <kjr.dev@hotmail.com>
2023-06-21 13:52:03 +0100

    completed cohort search

--- spec/cohortManager.spec.js
+++ spec/cohortManager.spec.js
@@ -14,4 +14,26 @@ describe('Cohorts', () => {
     // verify
     expect(res).toEqual(true)
   })
+
+  it('Returns the Cohort Data of a searched cohort', () => {
+    // setup
+    const cohortName = 'Cohort 1'
+    cohortManager.addCohort(cohortName)
+    // execute
+    const res = cohortManager.searchCohort(cohortName)
+    // verify
+    expect(res).toEqual(true)
+  })
+
+  // unhappy path
+  it('Returns the Cohort Does not exist when an invalid Cohort is inputted into the search', () => {
+    // setup
+    const cohortName = 'Cohort 1'
+    const invalidCohortName = 'Cohort 992'
+    cohortManager.addCohort(cohortName)
+    // execute
+    const res = cohortManager.searchCohort(invalidCohortName)
+    // verify
+    expect(res).toEqual(false)
+  })
 })
--- src/cohortManager.js
+++ src/cohortManager.js
@@ -11,6 +11,19 @@ class CohortManager {
     this.cohortList.push(cohortObj)
     return true
   }
+
+  searchCohort(cohortName) {
+    if (this.cohortList.find((cohort) => cohort.cohortName === cohortName)) {
+      console.log(
+        'Cohort Search Results:',
+        this.cohortList.find((cohort) => cohort.cohortName === cohortName)
+      )
+      return true
+    } else {
+      console.error('Cohort does not exist')
+      return false
+    }
+  }
 }
 
 module.exports = CohortManager
