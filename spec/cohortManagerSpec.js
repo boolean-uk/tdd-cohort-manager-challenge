1fd4fc7dccbca7d0b32594e472a206af5d06dd9f
davidbrockway <dav.brock@protonmail.com>
2023-06-21 17:15:06 +0100

    createCohort - TDD

--- spec/cohortManagerSpec.js
+++ spec/cohortManagerSpec.js
@@ -12,6 +12,11 @@ describe('Cohort Manager', () => {
     cohortA = cohortManager.createCohort('Cohort A');
     student1 = new Student(1, 'John', 'Doe', 'johndoe', 'john@example.com');
     student2 = new Student(2, 'Jane', 'Smith', 'janesmith', 'jane@example.com');
+});
+
+describe('createCohort', () => {
+    it('should create a new cohort with the given name', () => {
+      expect(cohortA.cohortName).toBe('Cohort A');
+      expect(cohortManager.cohorts.length).toBe(1);
     });
-  });
 
--- src/cohortManager.js
+++ src/cohortManager.js
@@ -4,4 +4,10 @@ class Cohort {
     this.students = []
   }
 
-  
+  addStudent(student) {
+    this.students.push(student)
+    console.log(
+      `Added student ${student.firstName} ${student.lastName} to ${this.cohortName}`
+    )
+  }
+
