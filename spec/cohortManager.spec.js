99a0af2c1388f6735c05fb0309cdc4d7a5965582
desokan <thefdmm@gmail.com>
2023-06-21 12:57:32 +0100

    refresh project

--- spec/cohortManager.spec.js
+++ spec/cohortManager.spec.js
@@ -1,4 +1,5 @@
-import CohortManager from '../src/cohortManager.js'
+// import CohortManager from '../src/cohortManager.js'
+const CohortManager = require('../src/cohortManager.js')
 
 describe('Cohort Manager Application', () => {
   let cmanager
@@ -14,5 +15,28 @@ describe('Cohort Manager Application', () => {
     const createCohort = cmanager.createNewCohort('Cohort 1')
     // THEN
     expect(createCohort).toEqual(newCohort)
+  })
+
+  it('should search cohort by cohort name', () => {
+    // GIVEN
+    const cohortName = 'Cohort 1'
+    // WHEN
+    cmanager.createNewCohort('Cohort 1')
+    const searchResult = cmanager.searchCohortByName(cohortName)
+    // THEN
+    expect(searchResult).toEqual({ cohortName: 'Cohort 1', students: [] })
+  })
+
+  it('should add student to a cohort', () => {
+    // GIVEN
+    // const student = {
+    //   studentID: 1,
+    //   firstname: 'a',
+    //   lastname: 'b',
+    //   gitusername: '@gitadesokan',
+    //   email: 'ade@gmail.com'
+    // }
+    // WHEN
+    // THEN
   })
 })
