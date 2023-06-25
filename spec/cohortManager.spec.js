cafe7b5e09671e086c6d81af15e9d46691a83f38
Lee Kelly <leeowenkelly@gmail.com>
2023-06-25 12:09:39 +0100

    addStudent created using ternary and passes test

--- spec/cohortManager.spec.js
+++ spec/cohortManager.spec.js
@@ -43,4 +43,49 @@ describe('Cohort Manager', () => {
       expect(result).toEqual(expected)
     })
   })
+
+  describe('addStudent', () => {
+    it('add a student to a specific cohort', () => {
+      // SETUP
+      cohortManager.addCohort('Cohort 1')
+      const expected = [
+        {
+          id: 1,
+          forename: 'Joe',
+          surname: 'Bloggs',
+          github: 'joebloggs23',
+          email: 'joebloggs23@gmail.com'
+        }
+      ]
+
+      // EXECUTE
+      const result = cohortManager.addStudent(
+        'Cohort 1',
+        'Joe',
+        'Bloggs',
+        'joebloggs23',
+        'joebloggs23@gmail.com'
+      )
+
+      //   RESULT
+      expect(result).toEqual(expected)
+    })
+
+    it('add student to non existant cohort', () => {
+      // SETUP
+      const expected = 'cohort does not exist'
+
+      // EXECUTE
+      const result = cohortManager.addStudent(
+        'Cohort 1',
+        'Joe',
+        'Bloggs',
+        'joebloggs23',
+        'joebloggs23@gmail.com'
+      )
+
+      //   RESULT
+      expect(result).toEqual(expected)
+    })
+  })
 })
--- src/cohortManager.js
+++ src/cohortManager.js
@@ -1,16 +1,33 @@
 class CohortManager {
   constructor() {
     this.cohorts = []
+    this.studentID = 0
   }
 
   addCohort(name) {
-    this.cohorts.push(name)
+    this.cohorts.push({ name: name })
     return `${name} created`
   }
 
   searchCohort(name) {
-    const findCohort = this.cohorts.find((cohortName) => cohortName === name)
+    const findCohort = this.cohorts.find((obj) => obj.name === name)
     return findCohort !== undefined ? `${name} found` : `${name} does not exist`
+  }
+
+  addStudent(cohort, forename, surname, username, email) {
+    const cohortIndex = this.cohorts.findIndex((obj) => obj.name === cohort)
+    return cohortIndex !== -1
+      ? (this.studentID++,
+        (this.cohorts[cohortIndex].students = []),
+        this.cohorts[cohortIndex].students.push({
+          id: this.studentID,
+          forename: forename,
+          surname: surname,
+          github: username,
+          email: email
+        }),
+        this.cohorts[cohortIndex].students)
+      : 'cohort does not exist'
   }
 }
 
