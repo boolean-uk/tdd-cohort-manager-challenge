558bd1a31b8168d65a8f2f996ee5a7117bf5bf44
Jack Miller <jdmiller1991@outlook.com>
2023-12-20 15:17:51 +0000

    Created latest function and test.

--- spec/cohort-spec.js
+++ spec/cohort-spec.js
@@ -1,10 +1,30 @@
 import Cohort from '../src/Cohort.js'
+import Student from '../src/Student.js'
 
 describe('cohort', () => {
   describe('creating a cohort', () => {
     it('should create a cohort with a given name', () => {
       const cohort = new Cohort('Cohort 1')
       expect(cohort.CohortName).toBe('Cohort 1')
+    })
+    //
+    it('searching for a cohort by name', () => {
+      const cohort1 = new Cohort('Cohort 1')
+      const cohortsArray = [cohort1]
+      const searchResult = cohort1.searchCohort('Cohort 1', cohortsArray)
+      expect(searchResult.CohortName).toBe('Cohort 1')
+    })
+    it('adding a new student to a cohort', () => {
+      const cohort = new Cohort('Cohort 1')
+      const student = new Student(
+        'Digby',
+        'Postman',
+        'Digman',
+        'Iimpregnate@gmail.com',
+        '1234'
+      )
+      cohort.addStudent(student)
+      expect(cohort.students.includes(student)).toBe(true)
     })
   })
 })
--- src/Cohort.js
+++ src/Cohort.js
@@ -1,6 +1,19 @@
+import Student from '../src/Student.js'
+
 class Cohort {
   constructor(name) {
     this.CohortName = name
+    this.students = []
+    this.cohort = []
+  }
+
+  addStudent(student) {
+    this.students.push(student)
+  }
+
+  searchCohort(name, cohortsArray) {
+    return cohortsArray.find((cohort) => cohort.CohortName === name)
   }
 }
+
 export default Cohort
