f5b071b53568b9672b8b1034fcd410ca67c965d8
Kye <NovusAa12@proton.me>
2023-12-20 15:38:27 +0000

    add tests for addStudentToCohort()

--- spec/cohortManager.spec.js
+++ spec/cohortManager.spec.js
@@ -50,4 +50,44 @@ describe('Cohort Manager:', () => {
       'Cohort does not exist'
     )
   })
+
+  it('add student to cohort', () => {
+    cohort.createCohort('cohort-1')
+
+    const expected = cohort.cohorts.find(
+      (cohort) => cohort.name === 'cohort-1'
+    ).students
+
+    const result = cohort.addStudentToCohort(
+      {
+        firstName: 'Kye',
+        lastName: 'Yee',
+        github: '@yee0802',
+        email: 'ky@mail.com'
+      },
+      'cohort-1'
+    )
+
+    expect(result).toEqual(expected)
+  })
+
+  it('should throw error if cohort is not found in addStudentToCohort', () => {
+    expect(() =>
+      cohort.addStudentToCohort(
+        {
+          firstName: 'Kye',
+          lastName: 'Yee',
+          github: '@yee0802',
+          email: 'ky@mail.com'
+        },
+        'cohort'
+      )
+    ).toThrowError('Cohort does not exist')
+  })
+
+  it('should throw error if student input is invalid', () => {
+    expect(() => cohort.addStudentToCohort({}, 'cohort-11')).toThrowError(
+      "Please input a valid student e.g. {firstName: 'John', lastName: 'Doe', github: '@johndoe', email:'johndoe@mail.com'}"
+    )
+  })
 })
