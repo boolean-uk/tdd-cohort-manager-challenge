84a56124ec6666271aab54901239d7e1c2dd49aa
Kye <NovusAa12@proton.me>
2023-12-20 13:37:21 +0000

    update cohortManager.js

--- src/cohortManager.js
+++ src/cohortManager.js
@@ -4,6 +4,10 @@ class CohortManager {
   }
 
   createCohort(cohortName) {
+    if (typeof cohortName !== 'string') {
+      throw new Error("please input a valid name e.g. 'cohort-11'")
+    }
+
     const duplicate = this.cohorts.some((cohort) => cohort.name === cohortName)
 
     if (!duplicate) {
@@ -16,6 +20,25 @@ class CohortManager {
       return `${cohortName} already exists`
     }
   }
+
+  findCohort(cohortName) {
+    if (typeof cohortName !== 'string') {
+      throw new Error("please input a valid name e.g. 'cohort-11'")
+    }
+
+    const hasCohort = this.cohorts.some((cohort) => cohort.name === cohortName)
+
+    if (hasCohort) {
+      return this.cohorts.find((cohort) => cohort.name === cohortName)
+    } else {
+      throw new Error('Cohort does not exist')
+    }
+  }
 }
+
+const c = new CohortManager()
+
+c.createCohort('cohort-11')
+console.log(c.findCohort('cohort-11'))
 
 export default CohortManager
