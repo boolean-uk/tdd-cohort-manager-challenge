8302a70ef599dc816bc558dd62cc2e5a2eb857cd
ilham saleh <ilham2saleh@gmail.com>
2023-12-20 13:56:07 +0000

    domain model for searching cohort created

--- spec/support/cohort.spec.js
+++ spec/support/cohort.spec.js
@@ -6,6 +6,7 @@ describe('Cohort', () => {
         cohort = new Cohort()
     })
 
+    // Creating a cohort
     it('creates a cohort with a name', () => {
         const cohortName = 'Cohort 1'
         cohort.createCohort(cohortName)
@@ -14,4 +15,5 @@ describe('Cohort', () => {
         expect(result[0].name).toEqual(cohortName)
     })
 
+    // Searching a cohort
 }) 
