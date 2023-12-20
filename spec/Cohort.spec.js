0daf6991268efe4225a4807cc6c81d9f5563b356
callumhayden <callum.hayden@gmail.com>
2023-12-20 12:24:47 +0000

    added a function that can search for a specific name

--- spec/Cohort.spec.js
+++ spec/Cohort.spec.js
@@ -1,10 +1,16 @@
 import Cohort from '../src/Cohort.js'
-
+// contains the whole cohort tests
 describe('cohort', () => {
-  describe('creating a cohort', () => {
-    it('should create a cohort with a given name', () => {
-      const cohort = new Cohort('Cohort 1')
-      expect(cohort.CohortName).toBe('Cohort 1')
-    })
+  // creating a new cohort with a specific name
+  it('should create a cohort with a given name', () => {
+    const cohort = new Cohort('Cohort 1')
+    expect(cohort.CohortName).toBe('Cohort 1')
+  })
+  // seraching for a cohort by the name
+  it('searching for a cohort by name', () => {
+    const cohort1 = new Cohort('Cohort 1')
+    const cohortsArray = [cohort1]
+    const searchResult = cohort1.searchCohort('Cohort 1', cohortsArray)
+    expect(searchResult.CohortName).toBe('Cohort 1')
   })
 })
--- src/Cohort.js
+++ src/Cohort.js
@@ -2,5 +2,10 @@ class Cohort {
   constructor(name) {
     this.CohortName = name
   }
+
+  searchCohort(name, cohortsArray) {
+    return cohortsArray.find((cohort) => cohort.CohortName === name)
+  }
 }
+
 export default Cohort
