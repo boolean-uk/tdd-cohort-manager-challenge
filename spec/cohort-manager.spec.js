838c90a9c12f4cc3c61a243cfc54bd0983fbaa31
Lukas Dembicki <lad_777@hotmail.com>
2023-12-20 16:52:33 +0000

    Add: find cohort - if no name was found spec.

--- spec/cohort-manager.spec.js
+++ spec/cohort-manager.spec.js
@@ -18,30 +18,37 @@ describe('Cohort Manger', () => {
       const expected = 'Cohort needs a name!'
       expect(() => cohortManager.createCohort('')).toThrowError(expected)
     })
-    describe('/ removeCohort', () => {
-      beforeEach(() => {
-        cohortManager.createCohort('KeepMe')
-        cohortManager.createCohort('RemoveMe')
-      })
-      it('/ removes a cohort', () => {
-        const expectation = 1
-        cohortManager.removeCohort('RemoveMe')
-        expect(cohortManager.cohortList.length).toEqual(expectation)
-      })
-      it('/ no parameter inputed', () => {
-        const expected = 'Please enter a cohort name'
-        expect(() => cohortManager.removeCohort('').toThrowError(expected))
-      })
+  })
+  describe('/ removeCohort', () => {
+    beforeEach(() => {
+      cohortManager.createCohort('KeepMe')
+      cohortManager.createCohort('RemoveMe')
     })
-    describe('find cohort', () => {
-      beforeEach(() => {
-        cohortManager.createCohort('findMe')
-      })
-      it('/ if found cohort', () => {
-        const expected = cohortManager.cohortList[0]
-        const result = cohortManager.findCohort('findMe')
-        expect(result).toEqual(expected)
-      })
+    it('/ removes a cohort', () => {
+      const expectation = 1
+      cohortManager.removeCohort('RemoveMe')
+      console.log(cohortManager.cohortList)
+      expect(cohortManager.cohortList.length).toEqual(expectation)
+    })
+    it('/ no parameter inputed', () => {
+      const expected = 'Please enter a cohort name'
+      expect(() => cohortManager.removeCohort('').toThrowError(expected))
+    })
+  })
+  describe('find cohort', () => {
+    beforeEach(() => {
+      cohortManager.createCohort('findMe')
+    })
+    it('/ if found cohort', () => {
+      const expected = cohortManager.cohortList[0]
+      const result = cohortManager.findCohort('findMe')
+      expect(result).toEqual(expected)
+    })
+    it('/ if no name was found', () => {
+      const expected = 'No cohort found with that name'
+      expect(() => cohortManager.findCohort('dontFindMe')).toThrowError(
+        expected
+      )
     })
   })
 })
