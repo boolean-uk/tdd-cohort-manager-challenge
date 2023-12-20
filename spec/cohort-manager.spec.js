365ad423346c87ac03c0e1e438fa2ac6be1c5582
Lukas Dembicki <lad_777@hotmail.com>
2023-12-20 15:37:11 +0000

    Add: createCohort() spec - no param.

--- spec/cohort-manager.spec.js
+++ spec/cohort-manager.spec.js
@@ -2,13 +2,21 @@ import CohortManager from '../src/cohort-manager.js'
 
 describe('Cohort Manger', () => {
   let cohortManager
+
   beforeEach(() => {
     cohortManager = new CohortManager()
   })
 
-  it('/ creates a new cohort', () => {
-    cohortManager.createCohort('Cohort 1')
-    const result = cohortManager.cohortList[0].name
-    expect(result).toEqual('Cohort 1')
+  describe('/ createCohort', () => {
+    it('/ creates a new cohort', () => {
+      cohortManager.createCohort('Cohort 1')
+      const result = cohortManager.cohortList[0].name
+      expect(result).toEqual('Cohort 1')
+    })
+
+    it('/ no parameter inputed', () => {
+      const expected = 'Cohort needs a name!'
+      expect(() => cohortManager.createCohort('')).toThrowError(expected)
+    })
   })
 })
