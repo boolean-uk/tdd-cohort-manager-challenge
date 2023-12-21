3aa13c1c19f3d8c3e0762360750c98b6770e3ea6
Lukas Dembicki <lad_777@hotmail.com>
2023-12-21 13:28:50 +0000

    Add: createStudent() method - adds a new student to allStudent list. TEST PASSING

--- src/cohort-manager.js
+++ src/cohort-manager.js
@@ -1,9 +1,12 @@
 import Cohort from './cohort.js'
-import { Student, countID } from './student.js'
+import { Student } from './student.js'
+
+let countID = 1
 
 class CohortManager {
   constructor() {
     this.cohortList = []
+    this.allStudents = []
   }
 
   createCohort(name) {
@@ -26,6 +29,13 @@ class CohortManager {
     if (!foundCohort) throw new Error('No cohort found with that name')
     return foundCohort
   }
+
+  createStudent(firstName, lastName, gitHub, email) {
+    const student = new Student(firstName, lastName, gitHub, email)
+    this.allStudents.push(student)
+    countID += 1
+    return student
+  }
 }
 
-export default CohortManager
+export { CohortManager, countID }
