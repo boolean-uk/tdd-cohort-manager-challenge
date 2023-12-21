ba04701f612717891ce7a9bff58f7d52899fc053
Pierluigi <pierluigi.capirci89@gmail.com>
2023-12-21 07:27:19 +0000

    create test if name already exists

--- spec/cohortManager.spec.js
+++ spec/cohortManager.spec.js
@@ -1,4 +1,3 @@
-// CohortManagerSpec.js
 import CohortManager from '../src/cohortManager.js'
 
 describe('CohortManager', () => {
@@ -10,5 +9,15 @@ describe('CohortManager', () => {
 
     expect(result.name).toBe(cohortName)
     expect(result.students).toEqual([])
+  })
+
+  it('should throw an error if cohort name is not unique', () => {
+    const cohortManager = new CohortManager()
+    const cohortName = 'cohort-11'
+
+    cohortManager.createCohort(cohortName)
+    expect(() => {
+      cohortManager.createCohort(cohortName)
+    }).toThrowError('Cohort name already exists')
   })
 })
--- src/cohortManager.js
+++ src/cohortManager.js
@@ -1,10 +1,16 @@
-// CohortManager.js
 class CohortManager {
   constructor() {
     this.cohorts = []
   }
 
   createCohort(cohortName) {
+    const existingCohort = this.cohorts.find(
+      (cohort) => cohort.name === cohortName
+    )
+
+    if (existingCohort) {
+      throw new Error('Cohort name already exists')
+    }
     const cohort = { name: cohortName, students: [] }
     this.cohorts.push(cohort)
     return cohort
