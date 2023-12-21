abff86b6befdf5a4ca3f25486708c7880508df88
Lukas Dembicki <lad_777@hotmail.com>
2023-12-21 13:50:23 +0000

    Removed: Student Class - default parameters moved to method.

--- src/student.js
+++ src/student.js
@@ -1,7 +1,7 @@
 import { countID } from './cohort-manager.js'
 
 class Student {
-  constructor(firstName, lastName = '', gitHub = '', email = '') {
+  constructor(firstName, lastName, gitHub, email) {
     this.studentId = countID
     this.firstName = firstName
     this.lastName = lastName
