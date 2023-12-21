96122bf0711f39a68ec4e2581c1e62738f16558b
Lukas Dembicki <lad_777@hotmail.com>
2023-12-21 12:59:36 +0000

    Add: student.js and Student class.

---
+++ src/student.js
@@ -0,0 +1,11 @@
+class Student {
+  constructor(studentID, firstName, lastName, gitHub, email) {
+    this.studentId = 0
+    this.firstName = firstName
+    this.lastname = lastName
+    this.gitHub = gitHub
+    this.email = email
+  }
+}
+
+export default Student
