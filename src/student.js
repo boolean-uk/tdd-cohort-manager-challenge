9a5cbdb42af72c1328b1c5a11457b86720e499be
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2023-12-21 13:18:43 +0100

    added the student.js file

---
+++ src/student.js
@@ -0,0 +1,12 @@
+class Student {
+  constructor(studentID, firstName, lastName, github, userName, email) {
+    this.studentID = studentID
+    this.firstName = firstName
+    this.lastName = lastName
+    this.github = github
+    this.email = email
+    this.userName = userName
+  }
+}
+
+export default Student
