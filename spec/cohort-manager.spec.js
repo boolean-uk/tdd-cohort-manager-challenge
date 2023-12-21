2559f4e37da889634783bbdb72bd28b46f3d6532
Lukas Dembicki <lad_777@hotmail.com>
2023-12-21 13:37:27 +0000

    Add: createStudent - only a name was added spec

--- spec/cohort-manager.spec.js
+++ spec/cohort-manager.spec.js
@@ -19,20 +19,24 @@ describe('Cohort Manger', () => {
       expect(() => cohortManager.createCohort('')).toThrowError(expected)
     })
   })
+
   describe('/ removeCohort', () => {
     beforeEach(() => {
       cohortManager.createCohort('KeepMe')
       cohortManager.createCohort('RemoveMe')
     })
+
     it('/ removes a cohort', () => {
       const expectation = 1
       cohortManager.removeCohort('RemoveMe')
       expect(cohortManager.cohortList.length).toEqual(expectation)
     })
+
     it('/ no parameter inputed', () => {
       const expected = 'Please enter a cohort name'
       expect(() => cohortManager.removeCohort('').toThrowError(expected))
     })
+
     it('/ no cohort was found', () => {
       const expected = 'No cohort found with that name'
       expect(() => cohortManager.removeCohort('ImNotHere')).toThrowError(
@@ -40,15 +44,18 @@ describe('Cohort Manger', () => {
       )
     })
   })
+
   describe('find cohort', () => {
     beforeEach(() => {
       cohortManager.createCohort('findMe')
     })
+
     it('/ if found cohort', () => {
       const expected = cohortManager.cohortList[0]
       const result = cohortManager.findCohort('findMe')
       expect(result).toEqual(expected)
     })
+
     it('/ if no name was found', () => {
       const expected = 'No cohort found with that name'
       expect(() => cohortManager.findCohort('dontFindMe')).toThrowError(
@@ -56,10 +63,12 @@ describe('Cohort Manger', () => {
       )
     })
   })
-  describe('/ createStudent method', () => {
+
+  describe('/ createStudent', () => {
     beforeEach(() => {
       cohortManager.allStudents = []
     })
+
     it('/ properly creates and adds a student to allStudent list', () => {
       const expectation = 'Steve'
       cohortManager.createStudent(
@@ -68,8 +77,15 @@ describe('Cohort Manger', () => {
         '/Stevyboy',
         'steve@hotmail.com'
       )
-      console.log(cohortManager.allStudents)
       expect(cohortManager.allStudents[0].firstName).toEqual(expectation)
+    })
+    it('/ only a name was added', () => {
+      const expectationOne = 'Steve'
+      const expecationTwo = ''
+      cohortManager.createStudent('Steve')
+      console.log(cohortManager.allStudents)
+      expect(cohortManager.allStudents[0].firstName).toEqual(expectationOne)
+      expect(cohortManager.allStudents[0].lastName).toEqual(expecationTwo)
     })
   })
 })
