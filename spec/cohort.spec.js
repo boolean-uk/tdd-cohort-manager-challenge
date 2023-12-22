11e21aada5c39ddd9f2749151b041b7e89c70494
irlydo <fokum8@gmail.com>
2023-12-22 12:26:14 +0000

    added second test

--- spec/cohort.spec.js
+++ spec/cohort.spec.js
@@ -14,4 +14,11 @@ describe('Manager', () => {
     manager.addCohort(cohort)
     expect(manager.cohorts.includes(cohort)).toBe(true)
   })
+
+  it('should find a cohort by name', () => {
+    const cohort = new Cohort('Cohort 1')
+    manager.addCohort(cohort)
+    const found = manager.findCohortByName('Cohort 1')
+    expect(found).toEqual(cohort)
+  })
 })
