a6d528db4439daec37f76da0aba7ced97f72b49e
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-12-22 08:19:18 +0000

    test: :test_tube: Student new instance missing details case

--- spec/student.spec.js
+++ spec/student.spec.js
@@ -21,5 +21,24 @@ describe('Student:', () => {
 
       expect(student.details).toEqual(expected)
     })
+
+    it('missing inputs', () => {
+      const inputTemplate = {
+        firstName: 'Bob',
+        lastName: 'Builder',
+        gitHubUserName: 'BobbyBuilder',
+        email: 'bob@builder.com',
+        cohortId: 7
+      }
+
+      for (const key in inputTemplate) {
+        const input = { ...inputTemplate }
+        delete input[key]
+
+        const callback = () => new Student(input)
+
+        expect(callback).toThrowError('student missing details')
+      }
+    })
   })
 })
