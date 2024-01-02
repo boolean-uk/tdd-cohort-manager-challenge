be673abaa16853ce819bcda9821303b2e4aef8a6
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-01-02 08:49:35 -0800

    prettier fix

--- src/CohortManager.js
+++ src/CohortManager.js
@@ -1,14 +1,14 @@
 class Cohort {
-    constructor(name) {
-        this.name = name
-        this.cohortList = []
-    }
+  constructor(name) {
+    this.name = name
+    this.cohortList = []
+  }
 
-    createCohortName(name) {
-        const cohortName = new Cohort(name)
-        this.cohortList.push(cohortName)
-        return cohortName
-    }
+  createCohortName(name) {
+    const cohortName = new Cohort(name)
+    this.cohortList.push(cohortName)
+    return cohortName
+  }
 }
 
 export default Cohort
