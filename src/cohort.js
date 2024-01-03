a2f69adff7d3d07d859eced1b8950c997dd9bfbd
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-03 14:55:27 +0100

    remove unneeded comments in cohort.js

--- src/cohort.js
+++ src/cohort.js
@@ -1,8 +1,3 @@
-// - Create a cohort with a cohort name
-// - Search for a cohort by cohort name
-// - Remove a cohort by cohort name
-// - Throw errors if student or cohort not found
-
 import { cohortCapacity } from '../src/constants.js'
 
 class Cohort {
