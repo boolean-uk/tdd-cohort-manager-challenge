73d43cc9e8fa8f3a33af7e34341510420781d7e0
Faiza <faraz_ul_islam@yahoo.com>
2024-01-03 16:05:44 +0000

    error fixed

--- src/cohort.js
+++ src/cohort.js
@@ -1,62 +1,57 @@
-import Student from './student.js';
+import Student from './student.js'
 class Cohort {
   constructor(id, cohortName) {
-    this.id = id;
-    this.cohortName = cohortName;
-    this.students = [];  // Ensure 'students' is initialized as an empty array
+    this.id = id
+    this.cohortName = cohortName
+    this.students = [] // Ensure 'students' is initialized as an empty array
   }
 
-
-
- 
- addStudent(studentID, firstName, lastName, githubUsername, email) {
+  addStudent(studentID, firstName, lastName, githubUsername, email) {
     const student = new Student(
       studentID,
       firstName,
       lastName,
       githubUsername,
       email
-    );
-    this.students.push(student);
-    return student; 
+    )
+    this.students.push(student)
+    return student
   }
 
-
   searchByName(Name) {
-    const studentSearched = this.students.find((student) => student.firstName === Name);
-    return studentSearched;
+    const studentSearched = this.students.find(
+      (student) => student.firstName === Name
+    )
+    return studentSearched
   }
 
   throwNotFoundError(message) {
-    throw new Error(message);
+    throw new Error(message)
   }
 
   removeStudent(Name) {
-    const foundIndex = this.students.findIndex((student) => student.firstName === Name);
+    const foundIndex = this.students.findIndex(
+      (student) => student.firstName === Name
+    )
     if (foundIndex >= 0) {
-      this.students.splice(foundIndex, 1);
+      this.students.splice(foundIndex, 1)
     } else {
-      this.throwNotFoundError('Student not found');
-   
+      this.throwNotFoundError('Student not found')
     }
   }
-  
 
-
-/*  removeStudent(studentID) {
+  /*  removeStudent(studentID) {
     const index = this.students.findIndex(student => student.studentID === studentID);
     if (index !== -1) {
       this.students.splice(index, 1);
     } else {
       throwNotFoundError('Student not found');
     }
-  }*/
-
+  } */
 
   getList() {
-    return this.students;
+    return this.students
   }
 }
 
-export default Cohort;
-
+export default Cohort
