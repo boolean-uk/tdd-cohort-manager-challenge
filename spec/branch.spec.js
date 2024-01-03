72280b6815edeb727ada1fa0191cb477c1647300
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-03 10:20:47 +0100

    rework addCohort, removeCohort to work with name instead of objects

--- spec/branch.spec.js
+++ spec/branch.spec.js
@@ -1,6 +1,5 @@
 import { Organization } from '../src/organization.js'
 import Branch from '../src/branch.js'
-import Cohort from '../src/cohort.js'
 
 describe('Branch', () => {
   let myOrganization
@@ -22,60 +21,50 @@ describe('Branch', () => {
   })
 
   describe('checking for cohorts', () => {
-    let myCohort
-    let mySecondCohort
     let myBranch
 
     beforeEach(() => {
       myBranch = new Branch('UK')
-      myCohort = new Cohort('#1')
-      mySecondCohort = new Cohort('#2')
-      myBranch.addCohort(myCohort)
+      myBranch.addCohort('#1')
     })
 
     it('returns true if cohort exists', () => {
-      expect(myBranch.cohortExists(myCohort)).toBeTrue()
+      expect(myBranch.cohortExists('#1')).toBeTrue()
     })
 
     it('returns true if cohort exists', () => {
-      expect(myBranch.cohortExists(mySecondCohort)).toBeFalse()
+      expect(myBranch.cohortExists('#2')).toBeFalse()
     })
   })
 
   describe('cohorts', () => {
-    let myCohort
     let myBranch
-
     beforeEach(() => {
-      myCohort = new Cohort('#2')
       myBranch = new Branch('UK')
+      myBranch.addCohort('#1')
     })
 
     describe('addition', () => {
-      beforeEach(() => {
-        myBranch.addCohort(myCohort)
-      })
       it('works if everything is fine', () => {
         expect(myBranch.cohorts.length).toEqual(1)
-        expect(myBranch.cohorts[0]).toEqual(myCohort)
+        expect(myBranch.cohorts[0].name).toEqual('#1')
       })
 
       it('of possible duplicates fails', () => {
         expect(myBranch.cohorts.length).toEqual(1)
-        expect(() => myBranch.addCohort(myCohort)).toThrowError('already exists')
+        expect(() => myBranch.addCohort('#1')).toThrowError('already exists')
         expect(myBranch.cohorts.length).toEqual(1)
       })
     })
 
     describe('removal', () => {
       it('works if cohort exists', () => {
-        myBranch.addCohort(myCohort)
-        myBranch.removeCohort('#2')
+        myBranch.removeCohort('#1')
         expect(myBranch.cohorts.length).toEqual(0)
       })
 
       it('does not work if cohort does not exist', () => {
-        expect(() => myBranch.removeCohort(myCohort)).toThrowError()
+        expect(() => myBranch.removeCohort('#2')).toThrowError()
       })
     })
   })
--- src/branch.js
+++ src/branch.js
@@ -1,26 +1,32 @@
+import Cohort from './cohort.js'
+
 class Branch {
   constructor(location) {
     this.location = location
     this.cohorts = []
   }
 
-  cohortExists(cohortObj) {
-    return !!this.cohorts.find((cohort) => cohort === cohortObj)
+  cohortExists(cohortName) {
+    return !!this.getCohortByName(cohortName)
   }
 
-  addCohort(cohortObj) {
-    if (this.cohortExists(cohortObj)) {
+  getCohortByName(cohortName) {
+    return this.cohorts.find((cohort) => cohort.name === cohortName)
+  }
+
+  addCohort(cohortName) {
+    const duplicates = this.getCohortByName(cohortName)
+    console.log(duplicates)
+    if (duplicates) {
       throw new Error('already exists')
     }
 
-    this.cohorts.push(cohortObj)
+    this.cohorts.push(new Cohort(cohortName))
     return this.cohorts
   }
 
   removeCohort(cohortName) {
-    const cohortToRemove = this.cohorts.find(
-      (cohort) => cohort.name === cohortName
-    )
+    const cohortToRemove = this.getCohortByName(cohortName)
     if (!cohortToRemove) {
       throw new Error('does not exist')
     }
