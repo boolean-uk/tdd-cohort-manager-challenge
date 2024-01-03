0cedda228c98779812792e43855c26aebf2eeccf
radio58 <rt58gm@gmail.com>
2024-01-03 09:49:34 +0000

    added createCohort()

--- src/manager.js
+++ src/manager.js
@@ -1,6 +1,24 @@
+import Cohort from "./cohort.js";
+
 class Manager {
     constructor(cohorts, students) {
-        this.cohorts = cohorts;
-        this.students = students;
+        this.cohorts = [];
+        this.students = [];
+    }
+
+    createCohort(cohortName) {
+        if (typeof cohortName !== 'string') return 'ERROR: cohortName is not a string'
+        const exists = this.cohorts.forEach(c => {
+            if (c.Name === cohortName) {
+                return true
+            }
+        });
+        if (exists) return 'ERROR: cohort already exists by that name'
+        
+        const cohort = { name: cohortName, students: [] }
+        this.cohorts.push(cohort)
+        return true
     }
 }
+
+export default Manager;
