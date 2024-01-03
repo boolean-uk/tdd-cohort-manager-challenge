f32c861ce37f00303b72619d14a0538dbb25150b
radio58 <rt58gm@gmail.com>
2024-01-03 12:10:09 +0000

    cleaned up and added addStudent()

--- src/cohort.js
+++ src/cohort.js
@@ -1,9 +1,21 @@
+import Student from "./student.js";
+
 class Cohort {
-    constructor(name, students) {
+    constructor(name) {
         this.name = name;
-        this.students = students;
+        this.students = [];
     }
-    // add/remove student methods
+    
+    addStudent(firstName, lastName, gitUsername, email) {
+        const exists = this.students.some((c) => c.email === email)
+
+        if (!exists) {
+            const {...student} = new Student(firstName, lastName, gitUsername, email)
+            this.students.push(student)
+        }
+        return true
+    }
+
 }
 
 export default Cohort;
