2feadd1fe8e48e599b0cae896317837bdd738aba
radio58 <rt58gm@gmail.com>
2024-01-03 12:43:47 +0000

    eslint formatting warcrimes

--- src/manager.js
+++ src/manager.js
@@ -1,44 +1,47 @@
-import Cohort from "./cohort.js";
+import Cohort from './cohort.js'
 
 class Manager {
-    constructor() {
-        this.cohorts = [];
-        this.students = [];
-    }
+  constructor() {
+    this.cohorts = []
+    this.students = []
+  }
 
-    createCohort(cohortName) {
-        if (typeof cohortName !== 'string') return 'ERROR: cohortName is not a string'
-        
-        const exists = this.cohorts.some(c => c.name === cohortName)
-        if (exists) return 'ERROR: cohort already exists'
-        
-        const {...cohort} = new Cohort(cohortName, [])
+  createCohort(cohortName) {
+    if (typeof cohortName !== 'string')
+      return 'ERROR: cohortName is not a string'
 
-        this.cohorts.push(cohort)
-        return true
-    }
+    const exists = this.cohorts.some((c) => c.name === cohortName)
+    if (exists) return 'ERROR: cohort already exists'
 
-    removeCohort(cohortName) {
-        if (typeof cohortName !== 'string') return 'ERROR: cohortName is not a string'
-        
-        const exists = this.cohorts.forEach((c, index) => {
-            if (c.name === cohortName) {
-                this.cohorts.splice(index, 1)
-            }
-        });
-        if (!exists) return `ERROR: cohort doesn't exist by that name`
+    const { ...cohort } = new Cohort(cohortName, [])
 
-        return true
-    }
+    this.cohorts.push(cohort)
+    return true
+  }
 
-    searchByCohortName(cohortName) {
-        if (typeof cohortName !== 'string') return 'ERROR: cohortName is not a string'
+  removeCohort(cohortName) {
+    if (typeof cohortName !== 'string')
+      return 'ERROR: cohortName is not a string'
 
-        const cohort = this.cohorts.find((c) => c.name === cohortName)
-        if (cohort !== undefined) return cohort
+    const exists = this.cohorts.forEach((c, index) => {
+      if (c.name === cohortName) {
+        this.cohorts.splice(index, 1)
+      }
+    })
+    if (!exists) return `ERROR: cohort doesn't exist by that name`
 
-        return 'ERROR: Cohort not found'
-    }
+    return true
+  }
+
+  searchByCohortName(cohortName) {
+    if (typeof cohortName !== 'string')
+      return 'ERROR: cohortName is not a string'
+
+    const cohort = this.cohorts.find((c) => c.name === cohortName)
+    if (cohort !== undefined) return cohort
+
+    return 'ERROR: Cohort not found'
+  }
 }
 
-export default Manager;
+export default Manager
