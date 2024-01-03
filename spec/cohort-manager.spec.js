998b91397b1f5dc07e2da440fbcf13807448f49d
radio58 <rt58gm@gmail.com>
2024-01-03 09:49:55 +0000

    added test for createCohort()

--- spec/cohort-manager.spec.js
+++ spec/cohort-manager.spec.js
@@ -1,5 +1,33 @@
-describe('first', () => {
-    it('first', () => {
+import Manager from "../src/manager.js"
 
+describe('manager', () => {
+    let manager
+
+    beforeEach(() => {
+        manager = new Manager
     })
+    
+    describe('creating a cohort', () => {
+        it('by name', () => {
+            const expected = [{ name: 'Cohort1', students: [] }]
+
+            manager.createCohort('Cohort1')
+
+            const result = manager.cohorts
+
+            expect(result).toEqual(expected)
+        })
+
+        it('by invalid name', () => {
+            const expected = []
+
+            manager.createCohort(1337)
+
+            const result = manager.cohorts
+
+            expect(result).toEqual(expected)
+        })
+    })
+
+
 })
