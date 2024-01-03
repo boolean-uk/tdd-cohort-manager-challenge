f798f299bad8f37ac97013bef1706410ec087396
radio58 <rt58gm@gmail.com>
2024-01-03 12:40:10 +0000

    cleaup

--- src/manager.js
+++ src/manager.js
@@ -10,10 +10,8 @@ class Manager {
         if (typeof cohortName !== 'string') return 'ERROR: cohortName is not a string'
         
         const exists = this.cohorts.some(c => c.name === cohortName)
-        
         if (exists) return 'ERROR: cohort already exists'
         
-        // const cohort = { name: cohortName, students: [] }
         const {...cohort} = new Cohort(cohortName, [])
 
         this.cohorts.push(cohort)
@@ -28,7 +26,6 @@ class Manager {
                 this.cohorts.splice(index, 1)
             }
         });
-        
         if (!exists) return `ERROR: cohort doesn't exist by that name`
 
         return true
@@ -38,7 +35,6 @@ class Manager {
         if (typeof cohortName !== 'string') return 'ERROR: cohortName is not a string'
 
         const cohort = this.cohorts.find((c) => c.name === cohortName)
-
         if (cohort !== undefined) return cohort
 
         return 'ERROR: Cohort not found'
