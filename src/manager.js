3df1d8534413544e07c1cf197a1ba3132bc3fd47
radio58 <rt58gm@gmail.com>
2024-01-03 10:07:08 +0000

    Added removeCohort()

--- src/manager.js
+++ src/manager.js
@@ -14,9 +14,21 @@ class Manager {
             }
         });
         if (exists) return 'ERROR: cohort already exists by that name'
-        
         const cohort = { name: cohortName, students: [] }
         this.cohorts.push(cohort)
+        return true
+    }
+
+    removeCohort(cohortName) {
+        if (typeof cohortName !== 'string') return 'ERROR: cohortName is not a string'
+        const exists = this.cohorts.forEach((c, index) => {
+            if (c.name === cohortName) {
+                this.cohorts.splice(index, 1)
+                console.log('spliced', this.cohorts)
+            }
+        });
+        if (!exists) return `ERROR: cohort doesn't exist by that name`
+
         return true
     }
 }
