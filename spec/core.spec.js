9903b97ff146728c14f9e5e06a0ed0fd932ae1ad
spectraldesign <spectraldesign1@gmail.com>
2024-02-19 10:03:22 +0100

    TestRemoveCohort

--- spec/core.spec.js
+++ spec/core.spec.js
@@ -19,4 +19,12 @@ describe('Test core criteria', () => {
     })
     expect(cohortManager.getCohort('cohort 2')).toBeUndefined()
   })
+
+  it('Should be able to remove cohort', function () {
+    cohortManager.addCohort('cohort 1')
+    cohortManager.addCohort('cohort 2')
+    cohortManager.removeCohort('cohort 1')
+    expect(cohortManager.cohorts).toEqual([{ name: 'cohort 2', students: [] }])
+    expect(() => cohortManager.removeCohort('cohort 1')).toThrow()
+  })
 })
