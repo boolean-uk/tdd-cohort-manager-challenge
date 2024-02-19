ef2cb08fa293bbc6701c37e4493d9ec676f3ba42
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-02-19 11:50:12 +0100

    Implement find by name function

--- spec/Standard.Spec.js
+++ spec/Standard.Spec.js
@@ -2,11 +2,15 @@ describe('Cohort Manager Standard Critera:', () => {
   let cohortManager
 
   beforeEach(() => {
-    delete require.cache[require.resolve('../src/CohortManager.js')]
     cohortManager = require('../src/CohortManager.js')
   })
 
   it('Creates a cohort with a cohort name', () => {
     expect(cohortManager.create('Test').name).toEqual('Test')
+  })
+
+  it('Finds a specific cohort by name', () => {
+    cohortManager.create('Test')
+    expect(cohortManager.find('Test')).toEqual({ name: 'Test', students: [] })
   })
 })
--- src/CohortManager.js
+++ src/CohortManager.js
@@ -1,11 +1,31 @@
+const cohorts = []
+
 /**
  * Creates a cohort with a given name
  * @returns A cohort object
  */
 function create(name) {
-  return { name: name }
+  const cohort = {
+    name: name,
+    students: []
+  }
+  cohorts.push(cohort)
+  return cohort
+}
+
+/**
+ * Finds a specific cohort by name
+ * @returns The cohort with the given name, otherwise null
+ */
+function find(name) {
+  const cohort = cohorts.find((p) => p.name === name)
+  if (cohort === undefined) {
+    return null
+  }
+  return cohort
 }
 
 module.exports = {
-  create: create
+  create: create,
+  find: find
 }
