ac0a5378113f7fd7fce95155e8f7e1ab522e48a5
Tim Zoltie <t.zoltie@gmail.com>
2024-06-03 11:18:28 +0100

    search cohort method and test created

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -11,4 +11,14 @@ describe('CohortManager', () => {
     expect(expected).toBeInstanceOf(Cohort)
     expect(expected.cohortName).toBe('Cohort 12')
   })
+  it('should search cohort by name', () => {
+    cohortManager.create('Cohort 10')
+    cohortManager.create('Cohort 11')
+    cohortManager.create('Cohort 12')
+
+    expect(cohortManager.search('Cohort 12').cohortName).toBe('Cohort 12')
+  })
+  it('should throw an error if the cohort does not exist', () => {
+    expect(() => cohortManager.search('Cohort 13')).toThrow('Cohort not found')
+  })
 })
--- src/index.js
+++ src/index.js
@@ -11,7 +11,18 @@ class CohortManager {
 
   create(name) {
     const newCohort = new Cohort(name)
+    this.cohorts.push(newCohort)
     return newCohort
+  }
+
+  search(name) {
+    const found = this.cohorts.find((c) => c.cohortName === name)
+
+    if (!found) {
+      // eslint-disable-next-line no-throw-literal
+      throw 'Cohort not found'
+    }
+    return found
   }
 }
 
