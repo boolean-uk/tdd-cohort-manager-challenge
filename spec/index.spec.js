10883e18f6e5fbe13472be3338c92b973755de67
Tim Zoltie <t.zoltie@gmail.com>
2024-06-03 13:12:41 +0100

    get all cohorts and students

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,10 +1,14 @@
-import CohortManager, { Cohort } from '../src/index.js'
+import CohortManager, { Cohort, Student } from '../src/index.js'
 
 describe('CohortManager', () => {
   let cohortManager
+  let cohort
+  let student
 
   beforeEach(() => {
     cohortManager = new CohortManager()
+    cohort = new Cohort()
+    student = new Student()
   })
   it('should create a new cohort', () => {
     const expected = cohortManager.create('Cohort 12')
@@ -20,5 +24,30 @@ describe('CohortManager', () => {
   })
   it('should throw an error if the cohort does not exist', () => {
     expect(() => cohortManager.search('Cohort 13')).toThrow('Cohort not found')
+  })
+  it('should create a new student and add it to the cohort', () => {
+    const expected = cohort.add(
+      'Troy',
+      'McClure',
+      'tmcclure',
+      'tmcclure@hollywood.org'
+    )
+    const result = new Student(
+      1,
+      'Troy',
+      'McClure',
+      'tmcclure',
+      'tmcclure@hollywood.org'
+    )
+    expect(expected).toBeInstanceOf(Student)
+    expect(expected).toEqual(result)
+  })
+  it('should get all cohorts', () => {
+    cohortManager.create('Cohort 11')
+    cohortManager.create('Cohort 12')
+    cohortManager.create('Cohort 13')
+
+    const expected = cohortManager.getAll()
+    expect(expected.length).toBe(3)
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
+class Student {
+  constructor(id, first, last, username, email) {
+    this.id = id
+    this.firstName = first
+    this.lastName = last
+    this.github = username
+    this.email = email
+  }
+}
+
 class Cohort {
   constructor(name) {
     this.cohortName = name
+    this.students = []
+    this.id = 1
+  }
+
+  add(first, last, username, email) {
+    const student = new Student(this.id, first, last, username, email)
+    this.id++
+    this.students.push(student)
+    return student
   }
 }
 
@@ -24,8 +43,18 @@ class CohortManager {
     }
     return found
   }
+
+  getAll() {
+    return this.cohorts
+  }
+
+  remove(name) {
+    const found = this.getAll()
+    const removed = found.filter((c) => c.cohortName !== name)
+    return removed
+  }
 }
 
-export { Cohort }
+export { Cohort, Student }
 
 export default CohortManager
