5290b06e2c1dc25343b9ae2cce4b14d5ec6d3096
Periklis <perik76@gmail.com>
2024-06-03 14:31:20 +0300

    Refactoring test to test for student instance created with firstName and lastName

--- spec/cohorts.spec.js
+++ spec/cohorts.spec.js
@@ -1,10 +1,10 @@
 import Student from '../src/student.js'
 
 describe('student', () => {
-  const student = new Student(1)
+  const student = new Student('Pickle Rick', 'Sanchez')
   it('should exist', () => {
-    expect(student.id).toBe(1)
-    expect(student.firstName).toBe('Bart')
-    expect(student.id).toBe(1)
+    expect(student.id).toBe(42)
+    expect(student.firstName).toBe('Pickle Rick')
+    expect(student.lastNametName).toBe('Sanchez')
   })
 })
