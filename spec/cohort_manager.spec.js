609f3a554d4821b720ec67fcc0094fbe433c9143
Periklis <perik76@gmail.com>
2024-06-03 22:38:29 +0300

    add test for cohort creation -fails

--- spec/cohort_manager.spec.js
+++ spec/cohort_manager.spec.js
@@ -1,4 +1,5 @@
 import Student from '../src/student.js'
+import Cohort from '../src/cohorts.js'
 
 describe('student', () => {
   const student = new Student('Pickle Rick', 'Sanchez')
@@ -8,5 +9,12 @@ describe('student', () => {
     expect(student.lastName).toBe('Sanchez')
     expect(student.githubUsername).toBe('picklerick')
     expect(student.email).toBe('picklerick@pickledimension.com')
+  })
+})
+
+describe('Cohort', () => {
+  const cohortOne = new Cohort(1)
+  it('should exist', () => {
+    expect(cohortOne.name).toBe(1)
   })
 })
---
+++ src/cohorts.js
@@ -0,0 +1,5 @@
+import student from './student.js'
+
+class Cohort {}
+
+export default Cohort
