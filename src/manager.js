3c678303390bd209d3983e12a33fb20bee45fbf8
Shaun Harris <shaun@shaunwilliamharris.com>
2024-06-03 15:11:00 +0100

    Student Passed

---
+++ src/manager.js
@@ -0,0 +1,11 @@
+class Student {
+  constructor() {
+    this.studentID = '1234'
+    this.firstName = 'John'
+    this.lastName = 'Doe'
+    this.githubUsername = 'codeDoe'
+    this.email = 'john_doe@boolean.com'
+  }
+}
+
+export { Student }
