269ae6249f480657b3673b640a448adbb7fc6e33
Leonardo Lodi <your_email@address>
2024-06-03 12:20:34 +0100

    test add student function

--- spec/CohortManager.spec.js
+++ spec/CohortManager.spec.js
@@ -24,4 +24,26 @@ describe('Cohort Manager', () => {
       new Cohort(1, 'Cohort 12')
     )
   })
+
+  it('should add student to a cohort', () => {
+    cohortManager.createCohort('Cohort 12')
+
+    expect(
+      cohortManager.addStudent(
+        'Leonardo',
+        'Lodi',
+        'LeonardoSaraceli',
+        'leonardolodi09@gmail.com',
+        1
+      )
+    ).toEqual(
+      new Cohort(1, 'Cohort 12', {
+        firstName: 'Leonardo',
+        lastName: 'Lodi',
+        gitHubUsername: 'LeonardoSaraceli',
+        email: 'leonardolodi09@gmail.com',
+        cohortID: 1
+      })
+    )
+  })
 })
