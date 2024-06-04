517680e963a8f6b284bc1a282b2111bd44d219ae
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-06-04 05:02:22 +0100

    create a cohort with a name test

--- spec/cohort.spec.js
+++ spec/cohort.spec.js
@@ -1,8 +1,19 @@
-import CohortManager from '../src/cohort.js'
+// eslint-disable-next-line no-unused-vars
+import CohortManager, { Cohort } from '../src/cohort.js'
 
 describe('CohortManager', () => {
+  let cohortManager
+
+  beforeEach(() => {
+    cohortManager = new CohortManager()
+  })
+
   it('should exist', () => {
-    const cohortManager = new CohortManager()
     expect(cohortManager).toBeInstanceOf(CohortManager)
+  })
+
+  it('should create a cohort with a cohort name', () => {
+    // eslint-disable-next-line no-unused-vars
+    const result = cohortManager.createCohort('class of 2024')
   })
 })
--- src/cohort.js
+++ src/cohort.js
@@ -1,5 +1,13 @@
-// eslint-disable-next-line prettier/prettier
 class CohortManager {
+  // eslint-disable-next-line prettier/prettier
+  createCohort(name) {
+    
+  }
+}
+
+// eslint-disable-next-line prettier/prettier
+class Cohort {
     
 }
+export { Cohort }
 export default CohortManager
