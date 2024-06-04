cb847ebe0294390c066eff9c2dd3609ee739ae90
samisalehsaeed <samisalehsaeed05@gmail.com>
2024-06-04 09:39:22 +0100

    3. Test to make sure there is students within said cohort and a test for adding a new student

--- spec/CohortManager.spec.js
+++ spec/CohortManager.spec.js
@@ -7,10 +7,26 @@ describe('Cohort Manager', () => {
     expect(cohort.getName()).toBeInstanceOf(String)
     expect(cohort.getName()).toBeTruthy()
   })
+
+  it('should have students', function () {
+    let cohort = new Cohort('test')
+    expect(cohort.students[0].id).toEqual(1)
+  })
+
   it('should search for a cohort by name', function () {
     let cohorts = new CohortManager([new Cohort('test')])
     let foundCohortName = cohorts.searchName('test')
     expect(typeof foundCohortName).toBe('string')
     expect(foundCohortName).toBeTruthy()
+  })
+  it('should add a student to a specific cohort', function () {
+    let cohort = new Cohort('test')
+    expect(cohort.studentAdded[0].id).toEqual(11)
+    expect(cohort.studentAdded[0].firstName).toEqual('Peter')
+    expect(cohort.studentAdded[0].lastName).toEqual('Parker')
+    expect(cohort.studentAdded[0].githubUsername).toEqual('peterparker123')
+    expect(cohort.studentAdded[0].email).toEqual(
+      'definitelynotspiderman@gmail.com'
+    )
   })
 })
--- src/Cohort.js
+++ src/Cohort.js
@@ -1,9 +1,96 @@
 export default class Cohort {
-  constructor(name) {
+  constructor(name, students = people, studentAdded = newStudent) {
     this.name = name
+    this.students = students
+    this.studentAdded = studentAdded
   }
 
   getName() {
     return this.name
   }
+
+  addStudent() {
+    return this.students
+  }
 }
+const newStudent = [
+  {
+    id: 11,
+    firstName: 'Peter',
+    lastName: 'Parker',
+    githubUsername: 'peterparker123',
+    email: 'definitelynotspiderman@gmail.com'
+  }
+]
+const people = [
+  {
+    id: 1,
+    firstName: 'Bart',
+    lastName: 'Simpson',
+    githubUsername: 'bartman1337',
+    email: 'bart.simpson@springfield.edu'
+  },
+  {
+    id: 2,
+    firstName: 'Lisa',
+    lastName: 'Simpson',
+    githubUsername: 'lisasaxophone',
+    email: 'lisa.simpson@springfield.edu'
+  },
+  {
+    id: 3,
+    firstName: 'Homer',
+    lastName: 'Simpson',
+    githubUsername: 'homercooldude',
+    email: 'homer.simpson@springfield.edu'
+  },
+  {
+    id: 4,
+    firstName: 'Eric',
+    lastName: 'Cartman',
+    githubUsername: 'cartmanrules',
+    email: 'eric.cartman@southpark.com'
+  },
+  {
+    id: 5,
+    firstName: 'Kyle',
+    lastName: 'Broflovski',
+    githubUsername: 'kyleman',
+    email: 'kyle.broflovski@southpark.com'
+  },
+  {
+    id: 6,
+    firstName: 'Stan',
+    lastName: 'Marsh',
+    githubUsername: 'stanmarsh',
+    email: 'stan.marsh@southpark.com'
+  },
+  {
+    id: 7,
+    firstName: 'Morty',
+    lastName: 'Smith',
+    githubUsername: 'morty_c137',
+    email: 'morty.smith@citadel.com'
+  },
+  {
+    id: 8,
+    firstName: 'Rick',
+    lastName: 'Sanchez',
+    githubUsername: 'ricksanchez',
+    email: 'rick.sanchez@citadel.com'
+  },
+  {
+    id: 9,
+    firstName: 'Jerry',
+    lastName: 'Smith',
+    githubUsername: 'jerry_bore',
+    email: 'jerry.smith@citadel.com'
+  },
+  {
+    id: 10,
+    firstName: 'Marge',
+    lastName: 'Simpson',
+    githubUsername: 'marge_simpson',
+    email: 'marge.simpson@springfield.edu'
+  }
+]
