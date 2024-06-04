cde4ea1107bb3030a8b30059ad0900f82c999538
Alistair Henshaw <alistairhenshaw@gmail.com>
2024-06-04 13:58:18 +0100

    tests made to check CohortManager exists and the ability to create a cohort

---
+++ spec/index.spec.js
@@ -0,0 +1,24 @@
+describe("CohortManager", () => {
+    let cohortManager
+
+    beforeEach(() => {
+        cohortManager = new CohortManager()
+    })
+
+    it("should exist", () => {
+        expect(cohortManager).toBeInstanceOf(CohortManager)
+    })
+
+    it("should be able to create a cohort", () => {
+        cohortManager.createCohort("alpha")
+        expect(cohortManager.cohorts).toEqual([{name: "alpha", students: []}])
+    })
+
+    it("should throw an error if the name isn't a string when creating a cohort", () => {
+        expect(cohortManager.createCohort(42)).toThrow("Name must be a string")
+    })
+
+    it("should throw an error if no name value given when creating a cohort", () => {
+        expect(cohortManager.createCohort()).toThrow("No value given")
+    })
+})
