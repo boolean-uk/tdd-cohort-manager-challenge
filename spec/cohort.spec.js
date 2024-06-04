b4b3fe61434ead84ffff1f6ebcc50a495a384c3e
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-06-04 15:20:40 +0200

    should search for a cohort by cohort name-y

--- spec/cohort.spec.js
+++ spec/cohort.spec.js
@@ -10,5 +10,21 @@ describe('CohortManeger', () => {
   it('should exist', () => {
     expect(cohortsManeger).toBeInstanceOf(CohortsManeger)
   })
+
+  it('should create a new cohort', () => {
+    cohortsManeger.createCohort('boolean-12')
+
+    expect(cohortsManeger.cohorts.length).toBe(1)
+    expect(cohortsManeger.cohorts[0].name).toBe('boolean-12')
+  })
+
+  it('should search for a cohort by cohort name', () => {
+    cohortsManeger.createCohort('boolean-11')
+    cohortsManeger.createCohort('boolean-12')
+
+    const result = cohortsManeger.findCohort('boolean-12')
+
+    expect(result.name).toBe('boolean-12')
+  })
   // ----
 })
--- src/cohort.js
+++ src/cohort.js
@@ -2,8 +2,27 @@ export default class CohortsManeger {
   constructor() {
     this.cohorts = []
   }
+
+  createCohort(cohortName) {
+    const newCohort = new Cohort(cohortName)
+    this.cohorts.push(newCohort)
+  }
+
+  findCohort(cohortName) {
+    const cohort = this.cohorts.find((cohort) => cohort.name === cohortName)
+
+    if (cohort instanceof Cohort === false)
+      throw new Error(this.getErrorMessage(cohortName))
+
+    return cohort
+  }
 }
 
-export class Cohort {}
+export class Cohort {
+  constructor(name) {
+    this.name = name
+    this.students = []
+  }
+}
 
 export class Student {}
