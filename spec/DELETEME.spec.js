b2c10cda7b2ecaac703ae8563f3375921ee939fa
homonoviscoding <mama.leye@gmail.com>
2024-06-26 17:22:46 +0100

    first passing test for creating a cohort

--- spec/DELETEME.spec.js
+++ spec/DELETEME.spec.js
@@ -1,4 +1,22 @@
 // DELETE THIS FILE AFTER YOU HAVE MADE YOUR FIRST COMMIT WHICH CONTAINS YOUR OWN TESTS AND CODE
-describe('DELETEME', () => {
-  it('DELETEME', () => {})
+import CohortManager, { Cohort } from '../src/DELETEME.js'
+describe('CohortManger', () => {
+  let cohort
+
+  beforeEach(() => {
+    cohort = new CohortManager()
+  })
+
+  it('should exist', () => {
+    expect(cohort).toBeInstanceOf(CohortManager)
+  })
+
+  it('should create a new cohort', () => {
+    const result = cohort.createCohort('cohort 12')
+
+    expect(result).toBeInstanceOf(Cohort)
+    expect(result.id).toBe(1)
+    expect(result.cohortName).toBe('cohort 12')
+
+  })
 })
--- src/DELETEME.js
+++ src/DELETEME.js
@@ -1,1 +1,65 @@
 // DELETE THIS FILE AFTER YOU HAVE MADE YOUR FIRST COMMIT WHICH CONTAINS YOUR OWN TESTS AND CODE
+class CohortManager {
+    constructor() {
+        this.cohorts = []
+    }
+
+    createCohort(cohortName) {
+        const newCohort = new Cohort(1, cohortName)
+        this.cohorts.push(newCohort)
+    }
+
+    searchCohort(cohortName) {
+
+    }
+
+    addStudentsToCohort(cohortName, student) {
+
+    }
+
+    removeCohort(cohortName) {
+
+    }
+
+    removeStudentFromCohort(student) {
+
+    }
+
+}
+
+class Student {
+    constructor (studentId, firstName, lastName, githubUsername, email) {
+        this.studentId = studentId
+        this.firstName = firstName
+        this.lastName = lastName
+        this.githubUsername = githubUsername
+        this.email = email
+    }
+}
+
+class Cohort {
+    constructor(id, cohortName) {
+        this.id = id
+        this.cohortName = cohortName
+        this.students = []
+    }
+
+    addStudent(student) {
+        this.students.push(student)
+    }
+
+    removeStudent(studentId) {
+        const index = this.students.findIndex(student => student.studentId === studentId)
+
+        if (index !== -1) {
+            this.students.splice(index, 1)
+        }
+        else {
+            throw new Error('student not found')
+        }
+    }
+}
+
+export default CohortManager
+
+export { Cohort }
