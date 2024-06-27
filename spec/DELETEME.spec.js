e270b2affd67e4ee1534e825c219c073af0be468
homonoviscoding <mama.leye@gmail.com>
2024-06-27 05:23:18 +0100

    third test passing test for creating multiple cohorts/ incrementing id

--- spec/DELETEME.spec.js
+++ spec/DELETEME.spec.js
@@ -1,22 +1,35 @@
 // DELETE THIS FILE AFTER YOU HAVE MADE YOUR FIRST COMMIT WHICH CONTAINS YOUR OWN TESTS AND CODE
 import CohortManager, { Cohort } from '../src/DELETEME.js'
-describe('CohortManger', () => {
-  let cohort
+describe("CohortManger", () => {
+  let cohortManager
 
   beforeEach(() => {
-    cohort = new CohortManager()
+    cohortManager = new CohortManager()
   })
 
-  it('should exist', () => {
-    expect(cohort).toBeInstanceOf(CohortManager)
+  it("should exist", () => {
+    expect(cohortManager).toBeInstanceOf(CohortManager)
   })
 
-  it('should create a new cohort', () => {
-    const result = cohort.createCohort('cohort 12')
+  it("should create a new cohort", () => {
+    const result = cohortManager.createCohort('cohort 12')
 
     expect(result).toBeInstanceOf(Cohort)
     expect(result.id).toBe(1)
     expect(result.cohortName).toBe('cohort 12')
 
+  })
+
+  it("should create multiple cohorts with incrementing id", () => {
+    const result2 = cohortManager.createCohort('cohort 13')
+    const result3 = cohortManager.createCohort('cohort 14')
+
+    expect(result2).toBeInstanceOf(Cohort)
+    expect(result2.id).toBe(1)
+    expect(result2.cohortName).toBe('cohort 13')
+
+    expect(result3).toBeInstanceOf(Cohort)
+    expect(result3.id).toBe(2)
+    expect(result3.cohortName).toBe('cohort 14')
   })
 })
--- src/DELETEME.js
+++ src/DELETEME.js
@@ -1,35 +1,9 @@
 // DELETE THIS FILE AFTER YOU HAVE MADE YOUR FIRST COMMIT WHICH CONTAINS YOUR OWN TESTS AND CODE
-class CohortManager {
-    constructor() {
-        this.cohorts = []
-    }
 
-    createCohort(cohortName) {
-        const newCohort = new Cohort(1, cohortName)
-        this.cohorts.push(newCohort)
-    }
-
-    searchCohort(cohortName) {
-
-    }
-
-    addStudentsToCohort(cohortName, student) {
-
-    }
-
-    removeCohort(cohortName) {
-
-    }
-
-    removeStudentFromCohort(student) {
-
-    }
-
-}
-
+let studentIdCounter = 1
 class Student {
-    constructor (studentId, firstName, lastName, githubUsername, email) {
-        this.studentId = studentId
+    constructor (firstName , lastName, githubUsername, email) {
+        this.studentId = String(studentIdCounter++)
         this.firstName = firstName
         this.lastName = lastName
         this.githubUsername = githubUsername
@@ -46,6 +20,8 @@ class Cohort {
 
     addStudent(student) {
         this.students.push(student)
+        // console.log(this.students)
+        return student
     }
 
     removeStudent(studentId) {
@@ -60,6 +36,73 @@ class Cohort {
     }
 }
 
+class CohortManager {
+    constructor() {
+        this.cohorts = []
+        this.id = 1
+    }
+
+    createCohort(cohortName) {
+        const newCohort = new Cohort(this.id, cohortName)
+        this.id++
+        this.cohorts.push(newCohort)
+        console.log(newCohort)
+        return newCohort
+    }
+
+    searchCohort(cohortName) {
+        const findCohort = this.cohorts.find(cohort => cohort.cohortName === cohortName)
+        if (findCohort) {
+            return findCohort
+        }
+        else {
+            throw new Error('cohort not found')
+        }
+    }
+
+    addStudentsToCohort(cohortName, student) {
+        const cohort = this.searchCohort(cohortName)
+        cohort.addStudent(student)
+        // this.studentId++
+        // console.log(student.lastName)
+        return student
+    }
+
+    removeCohort(cohortName) {
+        const cohortToRemove = this.cohorts.findIndex(c => c.cohortName === cohortName)
+        if (cohortToRemove !== -1) {
+            this.cohorts.splice(cohortToRemove, 1)
+        }
+        else {
+            throw new Error('cohort does not exist')
+        }
+    }
+
+    removeStudentFromCohort(cohortName, student) {
+        const cohort = this.searchCohort(cohortName)
+        cohort.removeStudent(student)
+    }
+
+}
+
 export default CohortManager
 
 export { Cohort }
+
+const student1 = new Student('mama', 'leye', 'homonoviscoding', 'mama.leye@gmail.com')
+const student2 = new Student('kyle', 'vann', 'kyle', 'kyle.van@gmail.com')
+const cohortManager = new CohortManager()
+const cohort12 = cohortManager.createCohort('cohort 12')
+
+// const cohort12 = new Cohort('cohort 12')
+// cohort12.addStudent(student1)
+
+// cohort1.createCohort('cohort 12')
+cohortManager.createCohort('cohort 13')
+cohortManager.addStudentsToCohort('cohort 12', student1)
+cohortManager.addStudentsToCohort('cohort 12', student2)
+cohortManager.addStudentsToCohort('cohort 13', student1)
+
+
+console.log(cohort12.students)
+
